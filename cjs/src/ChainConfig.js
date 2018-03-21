105be7e14157d6b4a10a4dd0879525c2cd7d3bf7
liuyi <yeahliuyi@163.com>
2018-03-21 17:05:57 +0800

    bts gph->jmc

---
+++ cjs/src/ChainConfig.js
@@ -0,0 +1,96 @@
+"use strict";
+
+exports.__esModule = true;
+var _this = void 0;
+
+var ecc_config = {
+    address_prefix: "JMC"
+};
+
+_this = {
+    core_asset: "JMC",
+    address_prefix: "JMC",
+    expire_in_secs: 15,
+    expire_in_secs_proposal: 24 * 60 * 60,
+    review_in_secs_committee: 24 * 60 * 60,
+    networks: {
+        BitShares: {
+            core_asset: "JMC",
+            address_prefix: "JMC",
+            chain_id: "8413bd1fbfb5382a0d1cd340f6b560070e58744a05908a98b4e875a3fcb79a84"
+        },
+        Muse: {
+            core_asset: "MUSE",
+            address_prefix: "MUSE",
+            chain_id: "8413bd1fbfb5382a0d1cd340f6b560070e58744a05908a98b4e875a3fcb79a84"
+        },
+        Test: {
+            core_asset: "TEST",
+            address_prefix: "TEST",
+            chain_id: "8413bd1fbfb5382a0d1cd340f6b560070e58744a05908a98b4e875a3fcb79a84"
+        },
+        Obelisk: {
+            core_asset: "GOV",
+            address_prefix: "FEW",
+            chain_id: "8413bd1fbfb5382a0d1cd340f6b560070e58744a05908a98b4e875a3fcb79a84"
+        }
+    },
+
+    /** Set a few properties for known chain IDs. */
+    setChainId: function setChainId(chain_id) {
+
+        var i = void 0,
+            len = void 0,
+            network = void 0,
+            network_name = void 0,
+            ref = void 0;
+        ref = Object.keys(_this.networks);
+
+        for (i = 0, len = ref.length; i < len; i++) {
+
+            network_name = ref[i];
+            network = _this.networks[network_name];
+
+            if (network.chain_id === chain_id) {
+
+                _this.network_name = network_name;
+
+                if (network.address_prefix) {
+                    _this.address_prefix = network.address_prefix;
+                    ecc_config.address_prefix = network.address_prefix;
+                }
+
+                // console.log("INFO    Configured for", network_name, ":", network.core_asset, "\n");
+
+                return {
+                    network_name: network_name,
+                    network: network
+                };
+            }
+        }
+
+        if (!_this.network_name) {
+            console.log("Unknown chain id (this may be a testnet)", chain_id);
+        }
+    },
+
+    reset: function reset() {
+        _this.core_asset = "JMC";
+        _this.address_prefix = "JMC";
+        ecc_config.address_prefix = "JMC";
+        _this.expire_in_secs = 15;
+        _this.expire_in_secs_proposal = 24 * 60 * 60;
+
+        console.log("Chain config reset");
+    },
+
+    setPrefix: function setPrefix() {
+        var prefix = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "JMC";
+
+        _this.address_prefix = prefix;
+        ecc_config.address_prefix = prefix;
+    }
+};
+
+exports.default = _this;
+module.exports = exports["default"];
